986cf2a4eb3352b5db164961f53428f7d66121e4
Juan I. Zumer <jizumer@gmail.com>
2022-11-03 19:52:33 +0100

    Deposit more than one amount

--- src/main/java/com/jizumer/Account.java
+++ src/main/java/com/jizumer/Account.java
@@ -8,10 +8,14 @@ import java.util.List;
 public class Account implements AccountService {
 
     private List<Pair<LocalDate, Integer>> transactions = new ArrayList<>();
-    private Integer balance = 0;
+    private List<Integer> balances = new ArrayList<>();
 
     public void deposit(LocalDate date, int amount) {
-        balance += amount;
+        Integer previousBalance = 0;
+        if (balances.size() != 0) {
+            previousBalance = balances.get(balances.size() - 1);
+        }
+        balances.add(previousBalance + amount);
         transactions.add(new Pair<>(date, amount));
     }
 
@@ -22,7 +26,8 @@ public class Account implements AccountService {
     public void printStatement() {
         System.out.println("Date\t\t||\tAmount\t||\tBalance");
         for (int i = 0; i < transactions.size(); i++) {
-            System.out.print(transactions.get(i).first().format(DateTimeFormatter.ofPattern("dd/MM/YYYY")) + "\t||\t" + transactions.get(i).second() + "\t||\t" + balance);
+            System.out.println(transactions.get(i).first().format(DateTimeFormatter.ofPattern("dd/MM/YYYY")) + "\t||\t" +
+                    transactions.get(i).second() + "\t||\t" + balances.get(i));
         }
     }
 }
--- src/test/java/com/jizumer/AccountTest.java
+++ src/test/java/com/jizumer/AccountTest.java
@@ -22,7 +22,23 @@ public class AccountTest {
 
         account.printStatement();
 
-        assertEquals("Date\t\t||\tAmount\t||\tBalance\n05/07/2022\t||\t1000\t||\t1000", inMemoryStream.toString());
+        assertEquals("Date\t\t||\tAmount\t||\tBalance\n05/07/2022\t||\t1000\t||\t1000\n", inMemoryStream.toString());
+    }
+
+    @Test
+    public void shouldDepositAMoreThanOneAmount() {
+        Account account = new Account();
+        account.deposit(LocalDate.of(2022, 7, 5), 1000);
+        account.deposit(LocalDate.of(2022, 8, 1), 1000);
+        account.deposit(LocalDate.of(2022, 9, 15), 1000);
+        ByteArrayOutputStream inMemoryStream = new ByteArrayOutputStream();
+        PrintStream statementStream = new PrintStream(inMemoryStream);
+        System.setOut(statementStream);
+
+
+        account.printStatement();
+
+        assertEquals("Date\t\t||\tAmount\t||\tBalance\n05/07/2022\t||\t1000\t||\t1000\n01/08/2022\t||\t1000\t||\t2000\n15/09/2022\t||\t1000\t||\t3000\n", inMemoryStream.toString());
     }
 
 }
