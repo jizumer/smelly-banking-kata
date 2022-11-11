63491c11023344f7da56ab65f1bdb931253cfea0
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-11-11 17:01:50 +0100

    Create new Bonus class

---
+++ src/main/java/com/jizumer/Bonus.java
@@ -0,0 +1,12 @@
+package com.jizumer;
+
+public class Bonus {
+
+    private final int year;
+    private final Double bonus;
+
+    public Bonus(int year, Double bonus) {
+        this.year = year;
+        this.bonus = bonus;
+    }
+}
